fbe7f8147e597771abd5b0e91c6f16f1a9b21f4b
Navinkumar <navikutty49@gmail.com>
2023-02-09 17:21:36 +0530

    Initial commit

--- OOPS with javascript/script.js
+++ OOPS with javascript/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// Instance method 
 const Person=function(Name,Age)
 {
 this.Name=Name;
@@ -7,3 +7,13 @@ this.Age=Age;
 }
 const navin=new Person('Navinkumar',18);
 console.log(navin);
+
+//1.New {} is created
+//2.function is called ,this={}
+//3.{} linked a prototype 
+//4.function automatically return
+
+const some = new Person('Navinkumar',9360154574);
+const more = new Person ('Nishvanth',9655822571);
+console.log(navin instanceof Person);  // instance will return either true or false
+
