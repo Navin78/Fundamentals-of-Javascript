e48a2d5585adffd4059bbe80d9782704bb8074c5
Navinkumar <navikutty49@gmail.com>
2023-03-01 13:31:49 +0530

    Modules deleted

--- Import and Export Modules/script.js
+++ Import and Export Modules/script.js
@@ -1,3 +1,5 @@
 //Importing the module 
-import './shoppingCart.js';
+import {shippingcart,totalprice as tp,totalQuantity as tq} from './shoppingCart.js';
 console.log('Importing the module');
+shippingcart('5','apple');
+console.log(tp,tq);
--- Import and Export Modules/shoppingCart.js
+++ Import and Export Modules/shoppingCart.js
@@ -1,4 +1,13 @@
 //Exporting the module
-
-
 console.log('Login form created');
+const shipping=[10];
+const cart=[];
+export const shippingcart=function(quantity,product)
+{
+    cart.push({quantity,product});
+    console.log(`${quantity} ${product} added in shippingcart`);
+};
+
+const totalprice=500;
+const totalQuantity=10;
+export{totalprice,totalQuantity};
