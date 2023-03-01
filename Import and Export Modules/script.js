e974e9d7740d2beb5112637474ac6364cb7f71f6
Navinkumar <navikutty49@gmail.com>
2023-03-01 13:33:42 +0530

    Modles deleted

--- Import and Export Modules/script.js
+++ Import and Export Modules/script.js
@@ -1,5 +1,85 @@
-//Importing the module 
-import {shippingcart,totalprice as tp,totalQuantity as tq} from './shoppingCart.js';
-console.log('Importing the module');
-shippingcart('5','apple');
-console.log(tp,tq);
+// //Importing the module 
+// import {shippingcart,totalprice as tp,totalQuantity as tq} from './shoppingCart.js';
+// console.log('Importing the module');
+// shippingcart('5','apple');
+// console.log(tp,tq);
+
+// Importing module
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// addToCart('bread', 5);
+// console.log(price, tq); this is for my coding
+
+console.log('Importing module');
+// console.log(shippingCost);
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price);
+
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
+
+console.log(cart);
+
+
+import cloneDeep from '../node_modules/lodash-es/cloneDeep.js';
+
+
+
+
+
+const cart1 = [
+  
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 52},
+    { product: 'fried', quantity: 152},
+];
+let test_array =cart1;
+
+for (let i = 0; i < test_array.length; i++){
+    var cc = test_array[i].quantity ;
+    console.log(cc);
+}
+
+
+// if(module.hot)
+// {
+// module.hot.accept();
+// }
+
+
+
+
+// const stateClone = Object.assign({}, state);
+// const stateDeepClone = cloneDeep(state);
+
+// state.user.loggedIn = false;
+// console.log(stateClone);
+
+// console.log(stateDeepClone);
+
+
+//Imperative in js 
+const arr=[2,4,6,8,10]
+const doubled=[]
+for (let i=0;i<arr.length;i++)
+doubled[i]=arr[i]*2;
+console.log(doubled);
+
+const persons = [
+  {firstname : "Malcom", lastname: "Reynolds"},
+  {firstname : "Kaylee", lastname: "Frye"},
+  {firstname : "Jayne", lastname: "Cobb"}
+];
+console.log(persons);
+
+const person=persons;
+for (let i=0;i<person.length;i++){
+  const test2=person[i].lastname;
+  console.log(test2);
+}
--- Import and Export Modules/shoppingCart.js
+++ Import and Export Modules/shoppingCart.js
@@ -1,13 +1,40 @@
 //Exporting the module
-console.log('Login form created');
-const shipping=[10];
-const cart=[];
-export const shippingcart=function(quantity,product)
-{
-    cart.push({quantity,product});
-    console.log(`${quantity} ${product} added in shippingcart`);
+// console.log('Login form created');
+// const shipping=[10];
+// const cart=[];
+// export const shippingcart=function(quantity,product)
+// { this my coding 
+//     cart.push({quantity,product});
+//     console.log(`${quantity} ${product} added in shippingcart`);
+// };
+
+// const totalprice=500;
+// const totalQuantity=10;
+// export{totalprice,totalQuantity};
+
+
+// Exporting module
+console.log('Exporting module');
+
+// Blocking code
+// console.log('Start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finish fetching users');
+
+const shippingCost = 10;
+export const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
 };
 
-const totalprice=500;
-const totalQuantity=10;
-export{totalprice,totalQuantity};
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as tq };
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
